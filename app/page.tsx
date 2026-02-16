e2797c38876b002d365fc8f30ce6bdb07ba3852e
p0nkx <lunafabianemmanuel@gmail.com>
2026-02-15 22:16:54 -0300

    prueba de cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
-      <h1 className="text-4xl font-bold text-blue-900 mb-4">
-        Productora de Seguros Elfab
+
+      <h1 className="text-6xl font-bold text-blue-900 mb-4">
+        PROXIMAMENTE WEB SEGUROS NAG
       </h1>
+      <h2 className="text-4xl font-bold text-blue-900 mb-4">
+        Productora de Seguros Elfab
+      </h2>
 
       <p className="text-lg text-gray-700 mb-6 text-center max-w-xl">
         Ofrecemos seguros de autos, motos, personas y responsabilidad civil.
